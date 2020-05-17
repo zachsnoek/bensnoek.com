30f91a7f3f1c70fae3cff1ef44436e4f70627d75
zachsnoek <dev@zachsnoek.com>
2020-05-17 16:53:57 -0400

    Add Netlify contact form

---
+++ js/index.js
@@ -0,0 +1,4 @@
+document.getElementById("contact-me-btn").addEventListener("click", () => {
+    document.getElementById("contact-me-btn").classList.toggle("hidden");
+    document.getElementById("contact-form").classList.toggle("hidden");
+});
